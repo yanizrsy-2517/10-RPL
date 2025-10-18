5326547f4bc93e7a621e42f12463fbd7883d128b
yanizrsy-2517 <gloucousm@gmail.com>
2025-10-18 01:33:06 -0700

    Add files via upload

---
+++ 10-RPL/KELAS/python/number.py
@@ -0,0 +1,16 @@
+ang1 = 17
+ang2 = 11
+
+print("Angka sebelum ditambah: " + str(ang1))
+
+# Cara 1
+# angka = ang1 + 3
+# print("Angka setelah ditambah: " + str(angka))
+
+# Cara 2
+ang1 += 3
+print("Angka setelah ditambah: " + str(ang1))
+
+# print("Hasil penjumlahan: " + str(ang1 + ang2))
+# print("Hasil persenan: " + str(ang1 % ang2))
+# print("Hasil perpangkatan: " + str(ang1 ** ang2))
---
+++ 10-RPL/KELAS/python/variables.py
@@ -0,0 +1,14 @@
+# angka = 17
+# berkoma = 1.1
+# nama = "Novi"
+
+# # print(angka + " " + berkoma)
+# # print(berkoma)
+# # print("Nama saya adalah " + nama)
+
+# print(type(angka))
+# print(type(berkoma))
+# print(type(nama))
+
+nama = input("Masukkan nama : ")
+print("Halo " + nama + ", Selamat datang di kelas Python!")

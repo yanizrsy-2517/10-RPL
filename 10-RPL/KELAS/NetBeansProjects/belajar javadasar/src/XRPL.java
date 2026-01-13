d89c64ca508e0bd5a51ad8dcb8c608a77a683313
yanizrsy-2517 <gloucousm@gmail.com>
2026-01-13 13:25:15 +0700

    Add files via upload

---
+++ 10-RPL/KELAS/NetBeansProjects/belajar javadasar/src/XRPL.java
@@ -0,0 +1,32 @@
+
+public class XRPL {
+    
+    
+    public static void main(String[] args){
+         int angka;
+         angka=10;
+         
+        System.out.println(angka);
+        System.out.println("angka yang anda inputkan: "+angka); 
+        angka=100;
+        System.out.println("angka terbaru adalah: "+angka);
+        
+        double desimal=10.355;
+        float pecahan=13.455f;
+             System.out.println(desimal);
+             System.out.println("angka pecahan yang diimputkan:" +pecahan);
+             
+             String nama="yani";
+             char abjad='a';
+                  System.out.println("nama");
+                  System.out.println("abjad");
+                  
+              boolean betul = true;
+              boolean salah = false;
+                   System.out.println("betul");
+                   System.out.println("salah");
+            //desimal= 100.235 ;
+            System.out.println("desimal");
+        }
+    
+}
